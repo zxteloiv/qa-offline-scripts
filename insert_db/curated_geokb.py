83ec10183e7833b61749cf3286bc44510c6a69df
zxteloiv <zxteloiv@gmail.com>
2017-07-19 00:38:14 +0800

    insert json data into mongodb

---
+++ insert_db/curated_geokb.py
@@ -0,0 +1,29 @@
+# coding: utf-8
+
+import sys, datetime
+import json
+
+import pymongo
+
+def get_mongo_col(db="poi_db", col="bd_search"):
+    client = pymongo.MongoClient()
+    db = client.get_database(db)
+    col = db.get_collection(col)
+    return col
+
+def insert_file(filename, dbname, colname):
+    print "get_mongo_col", dbname, colname
+    dbcol = get_mongo_col(dbname, colname)
+    for i, line in enumerate(open(filename)):
+        try:
+            data = json.loads(line.rstrip())
+        except:
+            print >> sys.stderr, line.rstrip()
+
+        objid = dbcol.insert(data)
+        print datetime.datetime.now(), i, data[u'ID'], objid
+        sys.stdout.flush()
+
+if __name__ == "__main__":
+    insert_file(sys.argv[1], sys.argv[2], sys.argv[3])
+
