987cd2ac95b87a373bdd0f8218bc9e901bc6e5a2
zxteloiv <zxteloiv@gmail.com>
2017-08-11 03:54:31 +0800

    add jp kb stat

--- curated_geokb/stat_kb.sh
+++ curated_geokb/stat_kb.sh
@@ -1,15 +1,26 @@
 #!/bin/bash
 
-ROOT=$1;
+COUNTRY=$1
+ROOT=$2;
 
-echo "==========景点===========";
+if [ "$1" == "cn" ]; then
+    echo "==========景点===========";
 
-find $ROOT -name "景点*.json" | xargs cat | jq 'keys' | awk -F '"' '{arr[$2]+=1}END{for(k in arr) {print k"\t"arr[k]}}' | sort -k2,2g;
+    find $ROOT -name "景点*.json" | xargs cat | jq 'keys' | awk -F '"' '{arr[$2]+=1}END{for(k in arr) {print k"\t"arr[k]}}' | sort -k2,2g;
 
-echo "==========餐厅===========";
+    echo "==========餐厅===========";
 
-find $ROOT -name "餐厅*.json" | xargs cat | jq 'keys' | awk -F '"' '{arr[$2]+=1}END{for(k in arr) {print k"\t"arr[k]}}' | sort -k2,2g;
+    find $ROOT -name "餐厅*.json" | xargs cat | jq 'keys' | awk -F '"' '{arr[$2]+=1}END{for(k in arr) {print k"\t"arr[k]}}' | sort -k2,2g;
 
-echo "==========酒店===========";
+    echo "==========酒店===========";
 
-find $ROOT -name "酒店*.json" | xargs cat | jq 'keys' | awk -F '"' '{arr[$2]+=1}END{for(k in arr) {print k"\t"arr[k]}}' | sort -k2,2g;
+    find $ROOT -name "酒店*.json" | xargs cat | jq 'keys' | awk -F '"' '{arr[$2]+=1}END{for(k in arr) {print k"\t"arr[k]}}' | sort -k2,2g;
+
+elif [ "$1" == "jp" ]; then
+
+    echo "==========餐厅===========";
+    find $ROOT -name "*.json" | xargs cat | jq 'keys' | awk -F '"' '{arr[$2]+=1}END{for(k in arr) {print k"\t"arr[k]}}' | sort -k2,2g;
+
+fi
+
+
